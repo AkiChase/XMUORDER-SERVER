132ba726fe43e313f87ae40d573305b64e8dca34
如初 <59353056+ruchuby@users.noreply.github.com>
2022-03-18 19:36:53 +0800

    custom success info

--- xmuorder_server/common.py
+++ xmuorder_server/common.py
@@ -15,3 +15,16 @@ class ErrorInfo:
             self.success = success
         if errno is not None:
             self.errno = errno
+
+
+class SuccessInfo:
+    """
+    自定义成功信息
+    """
+    msg: str
+    success: int = 1
+
+    def __init__(self, msg: str, success: int = None):
+        self.msg = msg
+        if success is not None:
+            self.success = success
