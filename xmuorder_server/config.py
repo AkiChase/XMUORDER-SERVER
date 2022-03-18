f50fbee49d688b4da0cc5d7746d943c9c7a7a790
如初 <59353056+ruchuby@users.noreply.github.com>
2022-03-18 19:29:07 +0800

    global settings

--- xmuorder_server/config.py
+++ xmuorder_server/config.py
@@ -1,12 +1,34 @@
+from functools import lru_cache
+
 from pydantic import BaseSettings
+
+from xmuorder_server.common import ErrorInfo
 
 
 class Settings(BaseSettings):
-    host: str
-    port: int = 3306
-    user: str
-    password: str
+    database_host: str
+    database_port: int = 3306
+    database_user: str
+    database_password: str
     database_name: str
+    server_host: str
+    server_port: int
+    secret_id: str
+    secret_key: str
 
     class Config:
         extra = "ignore"
+
+
+class GlobalSettings:
+    settings: Settings = None
+
+    @classmethod
+    def init(cls, _env_file):
+        cls.settings = Settings(_env_file=_env_file)
+
+    @classmethod
+    def get(cls) -> Settings:
+        if cls.settings is None:
+            raise Exception(ErrorInfo("settings not loaded"))
+        return cls.settings
