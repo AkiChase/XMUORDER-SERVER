d17e10e49a5cdb937f0a11fc6ff60d8cce1a0a34
如初 <59353056+ruchuby@users.noreply.github.com>
2022-03-18 19:28:46 +0800

    common:  custom error info

---
+++ xmuorder_server/common.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+
+class ErrorInfo:
+    """
+    自定义错误信息
+    """
+    msg: str
+    success: int = 0
+    errno: Optional[int]
+
+    def __init__(self, msg: str, success: int = None, errno: int = None):
+        self.msg = msg
+        if success is not None:
+            self.success = success
+        if errno is not None:
+            self.errno = errno
