d9a6622b3bac5324ad40c0f61aed179e0b8afcb8
如初 <59353056+ruchuby@users.noreply.github.com>
2022-03-21 18:26:45 +0800

    xmu login

--- xmuorder_server/routers/sms.py
+++ xmuorder_server/routers/sms.py
@@ -7,7 +7,6 @@ from typing import Optional, Set, List
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 from tencentcloud.common import credential
-from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.sms.v20210111 import sms_client, models
 
 from .. import dependencies
@@ -30,34 +29,41 @@ class SendSmsModel(BaseModel):
 @router.post("/send")
 async def send_sms(data: SendSmsModel, verify=Depends(dependencies.code_verify_aes_depend)):
     conn = Mysql.connect()
+    try:
+        cid_list = [f"'{x}'" for x in data.cID_list]
+        for x in cid_list:
+            if x.find(' ') > -1:
+                raise HTTPException(status_code=400, detail="cid_list invalid")
 
-    cid_list = [f"'{x}'" for x in data.cID_list]
-    for x in cid_list:
-        if x.find(' ') > -1:
-            raise HTTPException(status_code=400, detail="cid_list invalid")
+        #   过滤出需要发送的电话号码
+        sql = f'''
+        select c.cID, p.phone
+        from canteen c
+                 left join phone p on c.cID = p.cID
+        where c.cID in {f"({','.join(cid_list)})"};
+        '''
+        res = Mysql.execute_fetchall(conn, sql=sql)
+        phone_list = set([line[1] for line in res if line[1] is not None])
 
-    #   过滤出需要发送的电话号码
-    sql = f'''
-    select c.cID, p.phone
-    from canteen c
-             left join phone p on c.cID = p.cID
-    where c.cID in {f"({','.join(cid_list)})"};
-    '''
-    res = Mysql.execute_fetchall(conn, sql=sql)
-    phone_list = set([line[1] for line in res if line[1] is not None])
+        #   更新 lastSendMsgTime
+        sql = f'''
+        update canteen set lastSendMsgTime = NOW()
+        where cID in {f"({','.join(cid_list)})"};
+        '''
+        Mysql.execute_only(conn, sql)
 
-    #   更新 lastSendMsgTime
-    sql = f'''
-    update canteen set lastSendMsgTime = NOW()
-    where cID in {f"({','.join(cid_list)})"};
-    '''
-    Mysql.execute_only(conn, sql)
+        #   发送短信
+        res = send_message(list(phone_list), time1=data.time1, time2=data.time2)
+        return SuccessInfo(msg='短信请求发送成功', data={'SendStatusSet': res.SendStatusSet})
 
-    #   发送短信
-    return send_message(list(phone_list), time1=data.time1, time2=data.time2)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail="Message sending failed")
+    finally:
+        conn.close()
 
 
-def send_message(phone_list: List[str], time1: str, time2: str) -> SuccessInfo:
+def send_message(phone_list: List[str], time1: str, time2: str) -> models.SendSmsResponse:
     """
     使用模板发送商家及时处理订单短信提醒
     :param phone_list: 号码列表，注意每个号码+86
@@ -65,32 +71,26 @@ def send_message(phone_list: List[str], time1: str, time2: str) -> SuccessInfo:
     :param time2: 超过{2}分钟未处理，系统将为您自动拒单
     :return:
     """
-    try:
-        settings = GlobalSettings.get()
-        # 实例化一个认证对象，入参需要传入腾讯云账户密钥对secretId，secretKey。
-        cred = credential.Credential(settings.secret_id, settings.secret_key)
 
-        # 实例化要请求产品(以sms为例)的client对象 第二个参数为地域
-        client = sms_client.SmsClient(cred, "ap-guangzhou")
+    settings = GlobalSettings.get()
+    # 实例化一个认证对象，入参需要传入腾讯云账户密钥对secretId，secretKey。
+    cred = credential.Credential(settings.secret_id, settings.secret_key)
 
-        # 实例化一个请求对象，根据调用的接口和实际情况，可以进一步设置请求参数
-        req = models.SendSmsRequest()
+    # 实例化要请求产品(以sms为例)的client对象 第二个参数为地域
+    client = sms_client.SmsClient(cred, "ap-guangzhou")
 
-        # 短信应用ID: 短信SdkAppId在 [短信控制台] 添加应用后生成的实际SdkAppId，示例如1400006666
-        req.SmsSdkAppId = "1400647289"
-        # 短信签名内容: 使用 UTF-8 编码，必须填写已审核通过的签名，签名信息可登录 [短信控制台] 查看
-        req.SignName = "XMU智能点餐"
+    # 实例化一个请求对象，根据调用的接口和实际情况，可以进一步设置请求参数
+    req = models.SendSmsRequest()
 
-        # 模板 ID: 必须填写已审核通过的模板 ID。模板ID可登录 [短信控制台] 查看
-        req.TemplateId = "1334610"
-        # 模板参数: 若无模板参数，则设置为空
-        req.TemplateParamSet = [str(time1), str(time2)]
-        req.PhoneNumberSet = phone_list
+    # 短信应用ID: 短信SdkAppId在 [短信控制台] 添加应用后生成的实际SdkAppId，示例如1400006666
+    req.SmsSdkAppId = "1400647289"
+    # 短信签名内容: 使用 UTF-8 编码，必须填写已审核通过的签名，签名信息可登录 [短信控制台] 查看
+    req.SignName = "XMU智能点餐"
 
-        res = client.SendSms(req)
-        return SuccessInfo(msg='短信请求发送成功', data={'SendStatusSet': res.SendStatusSet})
+    # 模板 ID: 必须填写已审核通过的模板 ID。模板ID可登录 [短信控制台] 查看
+    req.TemplateId = "1334610"
+    # 模板参数: 若无模板参数，则设置为空
+    req.TemplateParamSet = [str(time1), str(time2)]
+    req.PhoneNumberSet = phone_list
 
-    except TencentCloudSDKException as err:
-        print('错误信息')
-        print(err)
-        raise HTTPException(status_code=400, detail="Message sending failed")
+    return client.SendSms(req)
--- xmuorder_server/routers/xmu.py
+++ xmuorder_server/routers/xmu.py
@@ -1,8 +1,13 @@
+import re
+
+import execjs
+import requests
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 
 from .. import dependencies, security
 from ..common import SuccessInfo, ErrorInfo
+from ..database import Mysql
 from ..security import AES
 
 router = APIRouter()
@@ -10,7 +15,7 @@ router = APIRouter()
 
 class BindModel(BaseModel):
     """
-    登录信息模版
+    绑定信息模版
     """
     id: str
     uid: str
@@ -28,30 +33,189 @@ class LoginModel(BaseModel):
 
 @router.post("/bind")
 async def xmu_bind(data: BindModel, verify=Depends(dependencies.code_verify_depend)):
-    key = 'ord' + data.ts[0:11] + 'er_'
-    iv = 're_' + data.ts[0:11] + 'dro'
-    user_id = AES.encrypt_aes(key, iv, src=data.id)
-    password = AES.encrypt_aes(key, iv, src=data.pw)
-    openid = AES.encrypt_aes(key, iv, src=data.uid)
+    """
+    通过统一身份认证账号密码绑定用户信息，储存至数据库，并返回基本信息
+    """
+    session = requests.session()
+    session.headers['user-agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" \
+                                    " AppleWebKit/537.36 (KHTML, like Gecko)" \
+                                    " Chrome/99.0.4844.74 Safari/537.36"
+    try:
+        key = 'ord' + data.ts[0:11] + 'er_'
+        iv = 're_' + data.ts[0:11] + 'dro'
+        account = AES.decrypt_aes(key, iv, en_src=data.id)
+        password = AES.decrypt_aes(key, iv, en_src=data.pw)
+        openid = AES.decrypt_aes(key, iv, en_src=data.uid)
 
-    # 通过账号密码爬取获取需要的信息
-    # 储存需要的信息到mysql（以openid为主键）
+        # 账号密码登录
+        login_by_pw(session, account=account, pw=password)
 
-    return SuccessInfo('bind success', data={
-        #   需要返回的信息 dict
-    })
+        # 获取基本信息
+        basic_info = get_basic_info(session)
 
-    raise HTTPException(status_code=400, detail=ErrorInfo('bind failed'))
+        info = {}
+        key_dict = {
+            '姓名': 'name', '学号': 'user_id', '学院': 'college', '现在年级': 'grade', '手机': 'phone'
+        }
+        for item in basic_info:
+            if item['title'] in key_dict:
+                info[key_dict[item['title']]] = item['initializationValue']['stringValue']
+
+        # 以openid为 key, iv 加密
+        key = openid[0:17]
+        iv = openid[-16:]
+        en_pw = AES.encrypt_aes(key, iv, password)
+
+        info['openid'] = openid
+        info['account'] = account
+        info['pw'] = en_pw
+
+        # 储存需要的信息到数据库
+        store_info(info)
+
+        # 退出登录
+        session_logout(session)
+
+        session.close()
+
+        return SuccessInfo('bind success', data={
+            'name': info['name'],
+            'college': info['college'],
+            'grade': info['grade'],
+            'user_id': info['user_id']
+        })
+
+    except Exception as e:
+        print(e)
+        session.close()
+        raise HTTPException(status_code=400, detail=ErrorInfo('bind failed'))
 
 
 @router.post("/login")
 async def xmu_login(data: LoginModel, verify=Depends(dependencies.code_verify_depend)):
-    key = 'ord' + data.ts[0:11] + 'er_'
-    iv = 're_' + data.ts[0:11] + 'dro'
-    openid = AES.encrypt_aes(key, iv, src=data.uid)
+    """
+    通过openid读取数据库，返回用户信息
+    """
+    try:
+        key = 'ord' + data.ts[0:11] + 'er_'
+        iv = 're_' + data.ts[0:11] + 'dro'
+        openid = AES.encrypt_aes(key, iv, src=data.uid)
+        return SuccessInfo('login success', data=read_info(openid))
 
-    return SuccessInfo('login success', data={
-        #   需要返回的信息 dict
-    })
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=ErrorInfo('login failed'))
 
-    raise HTTPException(status_code=400, detail=ErrorInfo('login failed'))
+
+def read_info(openid: str) -> dict:
+    conn = Mysql.connect()
+    try:
+        sql = "select user_id, name, college, grade from user where openid = %(openid)s limit 1;"
+        res = Mysql.execute_fetchone(conn, sql, openid=openid)
+        return {
+            'user_id': res[0],
+            'name': res[1],
+            'college': res[2],
+            'grade': res[3]
+        }
+    except Exception as e:
+        raise Exception(e)
+    finally:
+        conn.commit()
+        conn.close()
+
+
+def store_info(info: dict) -> None:
+    """
+    储存用户信息到数据库，若openid或学号存在则更新，否则insert
+    :param info: 信息字典
+    """
+    conn = Mysql.connect()
+    try:
+        sql = '''
+        insert into user (openid, user_id, account, pw, name, college, phone, grade)
+        VALUES (%(openid)s, %(user_id)s, %(account)s, %(pw)s, %(name)s, %(college)s, %(phone)s, %(grade)s)
+        ON DUPLICATE KEY UPDATE
+        openid=%(openid)s, user_id=%(user_id)s,
+        account=%(account)s, pw=%(pw)s,
+        name=%(name)s, college=%(college)s,
+        phone=%(phone)s, grade=%(grade)s;'''
+
+        Mysql.execute_fetchall(conn, sql, **info)
+    except Exception as e:
+        raise Exception(e)
+    finally:
+        conn.commit()
+        conn.close()
+
+
+def get_basic_info(session: requests.Session) -> dict:
+    """
+    获取学工系统上的基本信息
+    :param session: 已登录统一身份的session
+    :return: 所有信息字典
+    """
+
+    #  登录学工系统
+    url = 'https://ids.xmu.edu.cn/authserver/login?service=https://xmuxg.xmu.edu.cn/login/cas/xmu'
+    res = session.get(url, allow_redirects=False)
+
+    # 获取cookie： SAAS_U
+    url = res.headers['Location']
+    session.get(url, allow_redirects=False)
+
+    # 获取信息
+    url = 'https://xmuxg.xmu.edu.cn/api/information/my/infoSettings/STUDENT/1?distributary=owner'
+    res = session.get(url)
+    return res.json()['data']['components']
+
+
+def login_by_pw(session: requests.Session, account: str, pw: str) -> requests.Response:
+    """
+    通过账号密码登录
+    :param session: requests.Session
+    :param account: 账号
+    :param pw: 密码
+    :return: 统一身份登录响应
+    """
+    url = 'https://ids.xmu.edu.cn/authserver/login'
+
+    res = session.get(url, timeout=5)
+    assert res.status_code == 200
+
+    info = {}
+    for name in ('lt', 'dllt', 'execution', '_eventId', 'rmShown', 'pwdDefaultEncryptSalt'):
+        key = 'name' if name != 'pwdDefaultEncryptSalt' else 'id'
+        result = re.search(r'{}="{}" value="([\s\S]*?)"'.format(key, name), res.text)
+        assert result is not None, 'match failed'
+        info[name] = result.group(1)
+
+    with open(r'../lib/encrypt.js', 'r') as f:
+        js = f.read()
+    ctx = execjs.compile(js)
+    password = ctx.call('encryptAES', pw, info['pwdDefaultEncryptSalt'])
+
+    # 登录
+    data = {
+        'username': account,
+        'password': password,
+        'lt': info['lt'],
+        'dllt': info['dllt'],
+        'execution': info['execution'],
+        '_eventId': info['_eventId'],
+        'rmShown': info['rmShown']
+    }
+
+    login_res = session.post(url, data=data)
+    assert login_res.status_code == 200
+    return login_res
+
+
+def session_logout(session: requests.Session):
+    """
+    退出统一身份登录
+    :param session:已登录的session
+    :return:
+    """
+    url = 'https://ids.xmu.edu.cn/authserver/logout'
+    return session.get(url)
