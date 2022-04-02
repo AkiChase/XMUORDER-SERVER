add450168c443ab0fea00feed07394ec22cca63f
如初 <1003019131@qq.com>
2022-04-02 16:15:44 +0800

    modify model init

--- bin/main.py
+++ bin/main.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.split(os.path.abspath(os.path.dirname(__file__)))[0])
 
 from xmuorder_server import config
 from xmuorder_server.database import Mysql
-from xmuorder_server.routers import sms, xmu, statistics
+from xmuorder_server.routers import sms, xmu, statistics, printer
 from xmuorder_server.logger import Logger
 from xmuorder_server.scheduler import Scheduler
 from xmuorder_server.weixin.weixin import WeiXin
@@ -23,22 +23,23 @@ async def __init():
     #   logger初始化
     Logger.init(os.path.abspath(os.path.join(__file__, '../log/日志.log')))
 
+    #   配置文件读取
+    config.GlobalSettings.init(_env_file='../.env')
+
+    #   Mysql连接初始化
+    Mysql.init()
+
+    #   微信模块初始化
+    WeiXin.init()
+
     #   短信相关 路由
     app.include_router(sms.router, prefix="/sms")
     #   xmu绑定 路由
     app.include_router(xmu.router, prefix="/xmu")
     #   统计模块 路由
     app.include_router(statistics.router, prefix="/statistics")
-
-    #   配置文件读取
-    config.GlobalSettings.init(_env_file='../.env')
-    settings = config.GlobalSettings.get()
-
-    #   Mysql连接
-    Mysql.init(**settings.dict())
-
-    #   微信模块初始化
-    WeiXin.init()
+    #   云打印机模块 路由
+    app.include_router(printer.router, prefix="/printer")
 
     #   scheduler初始化, router模块需要的任务在模块__init中添加
     Scheduler.init()
